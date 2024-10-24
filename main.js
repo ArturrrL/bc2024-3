3621802e67474f77520a9b1cf2ce2eecc6627a3b
ArturrrLbc2024-3 <artik.levitskyi18@gmail.com>
2024-10-24 19:26:11 +0300

    part 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+const fs = require('fs');
 const { Command } = require('commander');
 const program = new Command();
-const fs = require('fs');
 
 
 program
@@ -18,13 +18,45 @@ if (!options.input) {
   process.exit(1);
 }
 
-
 if (!fs.existsSync(options.input)) {
   console.error('Cannot find input file');
   process.exit(1);
 }
 
-
 if (!options.output && !options.display) {
   process.exit(0);
 }
+
+const data = readJsonFile(options.input);
+
+function readJsonFile(path) {
+  
+    return JSON.parse(fs.readFileSync(path, 'utf8'));
+}
+const processedData = processData(data);
+
+function processData(data) {
+  let result = '';
+  data.forEach(entry => {
+    result += `${entry.exchangedate}: ${entry.rate}\n`;
+  });
+  return result;
+}
+if (options.display) {
+  console.log(processedData);
+}
+if (options.output) {
+  writeToFile(options.output, processedData);
+}
+
+function writeToFile(path, data) {
+  try {
+    fs.writeFileSync(path, data, 'utf8');
+    console.log(`Результати записано до  ${path}`);
+  } catch (error) {
+    console.error('Помилка:', error.message);
+  }
+}
+
+
+
